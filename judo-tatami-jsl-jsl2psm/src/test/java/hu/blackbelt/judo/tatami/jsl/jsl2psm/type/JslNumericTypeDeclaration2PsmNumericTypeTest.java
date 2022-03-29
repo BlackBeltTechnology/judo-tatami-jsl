6566409611786c7142085ba89b25e3f0a06d9ed9
Norbert Herczeg <noherczeg@gmail.com>
2022-03-29 19:24:32 +0200

    JNG-3646: numeric type under entities

--- judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/type/JslNumericTypeDeclaration2PsmNumericTypeTest.java
+++ judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/type/JslNumericTypeDeclaration2PsmNumericTypeTest.java
@@ -5,6 +5,8 @@ import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
 import hu.blackbelt.judo.meta.jsl.jsldsl.ModelDeclaration;
 import hu.blackbelt.judo.meta.jsl.jsldsl.runtime.JslDslModel;
 import hu.blackbelt.judo.meta.jsl.runtime.JslParser;
+import hu.blackbelt.judo.meta.psm.data.Attribute;
+import hu.blackbelt.judo.meta.psm.data.EntityType;
 import hu.blackbelt.judo.meta.psm.runtime.PsmModel;
 import hu.blackbelt.judo.meta.psm.type.NumericType;
 import hu.blackbelt.judo.tatami.jsl.jsl2psm.Jsl2PsmTransformationTrace;
@@ -32,8 +34,7 @@ import static hu.blackbelt.judo.meta.psm.runtime.PsmModel.buildPsmModel;
 import static hu.blackbelt.judo.tatami.jsl.jsl2psm.Jsl2Psm.Jsl2PsmParameter.jsl2PsmParameter;
 import static hu.blackbelt.judo.tatami.jsl.jsl2psm.Jsl2Psm.executeJsl2PsmTransformation;
 import static hu.blackbelt.judo.tatami.jsl.jsl2psm.TestUtils.allPsm;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class JslNumericTypeDeclaration2PsmNumericTypeTest {
@@ -93,7 +94,7 @@ public class JslNumericTypeDeclaration2PsmNumericTypeTest {
         }
 
         jslModel.saveJslDslModel(jslDslSaveArgumentsBuilder().file(new File(TARGET_TEST_CLASSES, testName + "-jsl.model")));
-        psmModel.savePsmModel(psmSaveArgumentsBuilder().file(new File(TARGET_TEST_CLASSES, testName + "-psm.model")));
+        psmModel.savePsmModel(psmSaveArgumentsBuilder().validateModel(false).file(new File(TARGET_TEST_CLASSES, testName + "-psm.model")));
     }
 
     private void transform() throws Exception {
@@ -135,5 +136,34 @@ public class JslNumericTypeDeclaration2PsmNumericTypeTest {
         assertEquals(myNumber.get().getName(), "MyNumber");
         assertEquals(myNumber.get().getPrecision(), 12);
         assertEquals(myNumber.get().getScale(), 5);
+    }
+
+    @Test
+    void testNumericTypeInEntity() throws Exception {
+        testName = "TestNumericTypeInEntity";
+
+        Optional<ModelDeclaration> model = parser.getModelFromStrings(
+                "NumericTypeInEntityModel",
+                List.of("model NumericTypeInEntityModel\n" +
+                        "\n" +
+                        "type numeric Height precision 3 scale 0\n" +
+                        "\n" +
+                        "entity Person {\n" +
+                        "\tfield Height height\n" +
+                        "}"
+                )
+        );
+
+        assertTrue(model.isPresent());
+
+        jslModel.addContent(model.get());
+        transform();
+
+        final Optional<NumericType> psmTypeHeight = allPsm(psmModel, NumericType.class).filter(n -> n.getName().equals("Height")).findFirst();
+        assertTrue(psmTypeHeight.isPresent());
+        final Optional<EntityType> psmEntityPerson = allPsm(psmModel, EntityType.class).filter(e -> e.getName().equals("Person")).findFirst();
+        assertTrue(psmEntityPerson.isPresent());
+        final Optional<Attribute> psmPersonHeightAttribute = psmEntityPerson.get().getAllAttributes().stream().filter(a -> a.getName().equals("height")).findFirst();
+        assertTrue(psmPersonHeightAttribute.isPresent());
     }
 }
