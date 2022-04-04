cfb710f58f8c7f822b1a5a905ba4e56383614031
rolandknobloch <roland.knobloch@blackbelt.hu>
2022-04-04 14:22:34 +0200

    JNG-3648 [UNFINISHED] Added binary type transformation. [UNFINISHED] Added binary type transformation test.

---
+++ judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/type/JslBinaryTypeDeclaration2PsmBinaryTypeTest.java
@@ -0,0 +1,189 @@
+package hu.blackbelt.judo.tatami.jsl.jsl2psm.type;
+
+import hu.blackbelt.epsilon.runtime.execution.api.Log;
+import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
+import hu.blackbelt.judo.meta.jsl.jsldsl.ModelDeclaration;
+import hu.blackbelt.judo.meta.psm.data.Attribute;
+import hu.blackbelt.judo.meta.psm.data.EntityType;
+import hu.blackbelt.judo.meta.psm.type.BinaryType;
+import hu.blackbelt.judo.tatami.jsl.jsl2psm.AbstractTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static hu.blackbelt.judo.tatami.jsl.jsl2psm.TestUtils.allPsm;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+public class JslBinaryTypeDeclaration2PsmBinaryTypeTest extends AbstractTest {
+    static final String TARGET_TEST_CLASSES = "target/test-classes/type/binary";
+
+    @Override
+    protected String getTargetTestClasses() {
+        return "target/test-classes/type/binary";
+    }
+
+    @Override
+    protected String getTest() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    protected Log createLog() {
+        return new Slf4jLog(log);
+    }
+
+    @BeforeAll
+    static void prepareTestFolders() throws IOException {
+        if (!Files.exists(Paths.get(TARGET_TEST_CLASSES))) {
+            Files.createDirectories(Paths.get(TARGET_TEST_CLASSES));
+        }
+    }
+
+    @Test
+    void testDeclaration() throws Exception {
+        testName = "TestDeclaration";
+
+        Optional<ModelDeclaration> model = parser.getModelFromStrings(
+                "DeclarationModel",
+                List.of("model DeclarationModel\n" +
+                        "\n" +
+                        "type binary Picture\n"
+                )
+        );
+
+        assertTrue(model.isPresent());
+
+        jslModel.addContent(model.get());
+        transform();
+
+        final Set<BinaryType> psmBinaries = psmModelWrapper.getStreamOfPsmTypeBinaryType().collect(Collectors.toSet());
+        assertEquals(1, psmBinaries.size());
+
+        final Optional<BinaryType> binary = psmBinaries.stream().filter(n -> n.getName().equals("Binary")).findFirst();
+        assertTrue(binary.isPresent());
+        assertEquals(binary.get().getName(), "Binary");
+    }
+
+    @Test
+    void testEntityMember() throws Exception {
+        testName = "TestEntityMember";
+
+        Optional<ModelDeclaration> model = parser.getModelFromStrings(
+                "EntityMemberModel",
+                List.of("model EntityMemberModel\n" +
+                        "\n" +
+                        "type binary Picture\n" +
+                        "\n" +
+                        "entity User {\n" +
+                        "\tfield Picture profilePicture\n" +
+                        "}"
+                )
+        );
+
+        assertTrue(model.isPresent());
+
+        jslModel.addContent(model.get());
+        transform();
+
+        final Optional<BinaryType> psmTypePicture = psmModelWrapper.getStreamOfPsmTypeBinaryType().filter(n -> n.getName().equals("Picture")).findFirst();
+        assertTrue(psmTypePicture.isPresent());
+        final Optional<EntityType> psmEntityUser = psmModelWrapper.getStreamOfPsmDataEntityType().filter(e -> e.getName().equals("User")).findFirst();
+        assertTrue(psmEntityUser.isPresent());
+        final Optional<Attribute> psmUserPictureAttribute = psmEntityUser.get().getAllAttributes().stream().filter(a -> a.getName().equals("profilePicture")).findFirst();
+        assertTrue(psmUserPictureAttribute.isPresent());
+    }
+
+    @Test
+    void testEntityMemberRequired() throws Exception {
+        testName = "TestEntityMemberRequired";
+
+        Optional<ModelDeclaration> model = parser.getModelFromStrings(
+                "EntityMemberRequiredModel",
+                List.of("model EntityMemberRequiredModel\n" +
+                        "\n" +
+                        "type binary Picture\n" +
+                        "\n" +
+                        "entity User {\n" +
+                        "\tfield required Picture profilePicture\n" +
+                        "}"
+                )
+        );
+
+        assertTrue(model.isPresent());
+
+        jslModel.addContent(model.get());
+        transform();
+
+        final Optional<EntityType> psmEntityUser = psmModelWrapper.getStreamOfPsmDataEntityType().filter(e -> e.getName().equals("User")).findFirst();
+        assertTrue(psmEntityUser.isPresent());
+        final Optional<Attribute> psmUserPictureAttribute = psmEntityUser.get().getAllAttributes().stream().filter(a -> a.getName().equals("profilePicture")).findFirst();
+        assertTrue(psmUserPictureAttribute.isPresent());
+        assertTrue(psmUserPictureAttribute.get().isRequired());
+    }
+
+    @Test
+    void testEntityMemberInheritance() throws Exception {
+        testName = "TestEntityMemberInheritance";
+
+        Optional<ModelDeclaration> model = parser.getModelFromStrings(
+                "EntityMemberInheritanceModel",
+                List.of("model EntityMemberInheritanceModel\n" +
+                        "\n" +
+                        "type binary Picture\n" +
+                        "\n" +
+                        "entity User {\n" +
+                        "\tfield Picture profilePicture\n" +
+                        "}\n" +
+                        "\n" +
+                        "entity AdminUser extends User {\n" +
+                        "}"
+                )
+        );
+
+        assertTrue(model.isPresent());
+
+        jslModel.addContent(model.get());
+        transform();
+
+        final Optional<EntityType> psmAdminUser = psmModelWrapper.getStreamOfPsmDataEntityType().filter(e -> e.getName().equals("AdminUser")).findFirst();
+        assertTrue(psmAdminUser.isPresent());
+        final Optional<Attribute> psmAdminUserProfilePictureAttribute = psmAdminUser.get().getAllAttributes().stream().filter(a -> a.getName().equals("profilePicture")).findFirst();
+        assertTrue(psmAdminUserProfilePictureAttribute.isPresent());
+    }
+    
+    @Test
+    void testEntityMemberIdentifier() throws Exception {
+        Optional<ModelDeclaration> model = parser.getModelFromStrings(
+                "EntityMemberIdentifierModel",
+                List.of("model EntityMemberIdentifierModel\n" +
+                        "\n" +
+                        "type binary Picture\n" +
+                        "\n" +
+                        "entity User {\n" +
+                        "\tidentifier Picture profilePicture\n" +
+                        "}"
+                )
+        );
+
+        assertTrue(model.isPresent());
+
+        jslModel.addContent(model.get());
+        transform();
+
+        final Optional<EntityType> psmEmail = allPsm(psmModel, EntityType.class).filter(e -> e.getName().equals("User")).findFirst();
+        assertTrue(psmEmail.isPresent());
+        final Optional<Attribute> psmUserPictureAttribute = psmEmail.get().getAllAttributes().stream().filter(a -> a.getName().equals("profilePicture")).findFirst();
+        assertTrue(psmUserPictureAttribute.isPresent());
+        assertTrue(psmUserPictureAttribute.get().isIdentifier());
+    }
+}
