5db04f710066d160305a719a4c82bf252cdbbe93
Norbert Herczeg <noherczeg@gmail.com>
2022-04-07 13:24:50 +0200

    JNG-3685: fix inheritance for default TO-s

--- judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/transferobject/JslEntityDeclaration2PsmDefaultTransferObjectTypeTest.java
+++ judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/transferobject/JslEntityDeclaration2PsmDefaultTransferObjectTypeTest.java
@@ -8,6 +8,7 @@ import hu.blackbelt.judo.meta.psm.data.EntityType;
 import hu.blackbelt.judo.meta.psm.namespace.NamedElement;
 import hu.blackbelt.judo.meta.psm.service.MappedTransferObjectType;
 import hu.blackbelt.judo.meta.psm.service.TransferAttribute;
+import hu.blackbelt.judo.meta.psm.type.NumericType;
 import hu.blackbelt.judo.meta.psm.type.StringType;
 import hu.blackbelt.judo.tatami.jsl.jsl2psm.AbstractTest;
 import lombok.extern.slf4j.Slf4j;
@@ -61,14 +62,18 @@ public class JslEntityDeclaration2PsmDefaultTransferObjectTypeTest extends Abstr
                 List.of("model DefaultTransferObjectTypeModel\n" +
                         "\n" +
                         "type string Name max-length 32\n" +
+                        "type numeric Age precision 3 scale 0\n" +
                         "\n" +
                         "entity Test {\n" +
                         "\tfield Name test\n" +
                         "}\n" +
+                        "entity PersonWithAge extends Test {\n" +
+                        "\tfield Age age\n" +
+                        "}\n" +
                         "entity abstract Person {\n" +
                         "\tfield Name name\n" +
                         "}\n" +
-                        "entity SalesPerson extends Person {\n" +
+                        "entity SalesPerson extends Person, PersonWithAge {\n" +
                         "}\n"
                 )
         );
@@ -81,19 +86,21 @@ public class JslEntityDeclaration2PsmDefaultTransferObjectTypeTest extends Abstr
         final Optional<StringType> psmTypeName = psmModelWrapper.getStreamOfPsmTypeStringType().filter(n -> n.getName().equals("Name")).findFirst();
         assertTrue(psmTypeName.isPresent());
 
+        final Optional<NumericType> psmTypeAge = psmModelWrapper.getStreamOfPsmTypeNumericType().filter(n -> n.getName().equals("Age")).findFirst();
+        assertTrue(psmTypeAge.isPresent());
+
         final Set<EntityType> psmEntityTypes = psmModelWrapper.getStreamOfPsmDataEntityType().collect(Collectors.toSet());
-        assertEquals(3, psmEntityTypes.size());
+        assertEquals(4, psmEntityTypes.size());
 
         final Set<MappedTransferObjectType> psmTOTypes = psmModelWrapper.getStreamOfPsmServiceMappedTransferObjectType().collect(Collectors.toSet());
-        assertEquals(3, psmTOTypes.size());
-
-        final Set<String> psmTOTypeNames = psmTOTypes.stream().map(NamedElement::getName).collect(Collectors.toSet());
-        final Set<String> jslEntityTypeDeclarationNames = ImmutableSet.of("Test", "Person", "SalesPerson");
-        assertThat(psmTOTypeNames, IsEqual.equalTo(jslEntityTypeDeclarationNames.stream().map(n -> n + "DefaultTransferObject").collect(Collectors.toSet())));
+        assertEquals(4, psmTOTypes.size());
 
         final Optional<MappedTransferObjectType> psmDefaultTOPerson = psmTOTypes.stream().filter(e -> e.getName().equals("PersonDefaultTransferObject")).findAny();
         assertTrue(psmDefaultTOPerson.isPresent());
         assertTrue(psmDefaultTOPerson.get().isAbstract());
+
+        final Optional<MappedTransferObjectType> psmDefaultTOPersonWithAge = psmTOTypes.stream().filter(e -> e.getName().equals("PersonWithAgeDefaultTransferObject")).findAny();
+        assertTrue(psmDefaultTOPersonWithAge.isPresent());
 
         final Optional<MappedTransferObjectType> psmDefaultTOTest = psmTOTypes.stream().filter(e -> e.getName().equals("TestDefaultTransferObject")).findAny();
         assertTrue(psmDefaultTOTest.isPresent());
@@ -101,15 +108,28 @@ public class JslEntityDeclaration2PsmDefaultTransferObjectTypeTest extends Abstr
         final List<TransferAttribute> psmTestDefaultTOAttributes = psmDefaultTOTest.get().getAttributes();
         assertEquals(1, psmTestDefaultTOAttributes.size());
 
-        final TransferAttribute psmDefaultTOAttributeTest = psmTestDefaultTOAttributes.get(0);
-        assertEquals("test", psmDefaultTOAttributeTest.getName());
-        assertFalse(psmDefaultTOAttributeTest.isRequired());
-        assertEquals(psmTypeName.get(), psmDefaultTOAttributeTest.getDataType());
+        final Optional<MappedTransferObjectType> psmDefaultTOSalesPerson = psmTOTypes.stream().filter(e -> e.getName().equals("SalesPersonDefaultTransferObject")).findAny();
+        assertTrue(psmDefaultTOSalesPerson.isPresent());
 
-//        final Optional<MappedTransferObjectType> psmDefaultTOSalesPerson = psmTOTypes.stream().filter(e -> e.getName().equals("SalesPersonDefaultTransferObject")).findAny();
-//        assertTrue(psmDefaultTOSalesPerson.isPresent());
-//
-//        final List<TransferAttribute> psmSalesPersonAttributes = psmDefaultTOSalesPerson.get().getAttributes();
-//        assertEquals(1, psmSalesPersonAttributes.size());
+        final List<TransferAttribute> psmSalesPersonAttributes = psmDefaultTOSalesPerson.get().getAttributes();
+        assertEquals(3, psmSalesPersonAttributes.size());
+
+        final Optional<TransferAttribute> psmNameAttributeOnSalesPersonTO = psmSalesPersonAttributes.stream().filter(a -> a.getName().equals("name")).findFirst();
+        assertTrue(psmNameAttributeOnSalesPersonTO.isPresent());
+        assertEquals("name", psmNameAttributeOnSalesPersonTO.get().getName());
+        assertFalse(psmNameAttributeOnSalesPersonTO.get().isRequired());
+        assertEquals(psmTypeName.get(), psmNameAttributeOnSalesPersonTO.get().getDataType());
+
+        final Optional<TransferAttribute> psmTestAttributeOnSalesPersonTO = psmSalesPersonAttributes.stream().filter(a -> a.getName().equals("test")).findFirst();
+        assertTrue(psmTestAttributeOnSalesPersonTO.isPresent());
+        assertEquals("test", psmTestAttributeOnSalesPersonTO.get().getName());
+        assertFalse(psmTestAttributeOnSalesPersonTO.get().isRequired());
+        assertEquals(psmTypeName.get(), psmTestAttributeOnSalesPersonTO.get().getDataType());
+
+        final Optional<TransferAttribute> psmAgeAttributeOnSalesPersonTO = psmSalesPersonAttributes.stream().filter(a -> a.getName().equals("age")).findFirst();
+        assertTrue(psmAgeAttributeOnSalesPersonTO.isPresent());
+        assertEquals("age", psmAgeAttributeOnSalesPersonTO.get().getName());
+        assertFalse(psmAgeAttributeOnSalesPersonTO.get().isRequired());
+        assertEquals(psmTypeAge.get(), psmAgeAttributeOnSalesPersonTO.get().getDataType());
     }
 }
