fa4f6727b5f025f1854773abd3d8808624acd80e
Robert Csakany <robert.csakany@blackbelt.hu>
2022-04-14 18:09:53 +0200

    JNG-3672 Tranformation made, tests are failing

---
+++ judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/derived/JslEntityDerivedRelation2PsmRelationTest.java
@@ -0,0 +1,67 @@
+package hu.blackbelt.judo.tatami.jsl.jsl2psm.derived;
+
+import hu.blackbelt.epsilon.runtime.execution.api.Log;
+import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
+import hu.blackbelt.judo.meta.psm.derived.DataProperty;
+import hu.blackbelt.judo.tatami.jsl.jsl2psm.AbstractTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+public class JslEntityDerivedRelation2PsmRelationTest extends AbstractTest {
+    static final String TARGET_TEST_CLASSES = "target/test-classes/derived/derivedRelation";
+
+    @Override
+    protected String getTargetTestClasses() {
+        return TARGET_TEST_CLASSES;
+    }
+
+    @Override
+    protected String getTest() {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    protected Log createLog() {
+        return new Slf4jLog(log);
+    }
+
+    @BeforeAll
+    static void prepareTestFolders() throws IOException {
+        if (!Files.exists(Paths.get(TARGET_TEST_CLASSES))) {
+            Files.createDirectories(Paths.get(TARGET_TEST_CLASSES));
+        }
+    }
+
+    @Test
+    void testDerivedRelationDeclarationModel() throws Exception {
+        testName = "TestDerivedRelationModel";
+
+        jslModel = parser.getModelFromFiles(
+                "DerivedRelationModel",
+                List.of(new File("src/test/resources/derived/TestDerivedRelationModel.jsl"))
+        );
+
+        transform();
+
+        final Set<DataProperty> dataProperties = psmModelWrapper.getStreamOfPsmDerivedDataProperty().collect(Collectors.toSet());
+        assertEquals(1, dataProperties.size());
+        final Optional<DataProperty> valueProperty = dataProperties.stream().filter(n -> n.getName().equals("value")).findFirst();
+        assertTrue(valueProperty.isPresent());
+        assertTrue(valueProperty.get().isPrimitive());
+        assertEquals("self.leads!count()", valueProperty.get().getGetterExpression().getExpression());
+    }
+}
