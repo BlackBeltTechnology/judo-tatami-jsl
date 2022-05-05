b08c9f6b5cff29310d4760a65a3f24f38a3f607e
Norbert Herczeg <noherczeg@gmail.com>
2022-05-05 15:59:59 +0200

    JNG-3658: fix review comments

--- judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/entity/JslEntityDefaultValue2PsmPrimitiveAccessorTest.java
+++ judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/entity/JslEntityDefaultValue2PsmPrimitiveAccessorTest.java
@@ -3,6 +3,7 @@ package hu.blackbelt.judo.tatami.jsl.jsl2psm.entity;
 import hu.blackbelt.epsilon.runtime.execution.api.Log;
 import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
 import hu.blackbelt.judo.meta.psm.derived.PrimitiveAccessor;
+import hu.blackbelt.judo.meta.psm.type.FlatPrimitiveType;
 import hu.blackbelt.judo.tatami.jsl.jsl2psm.AbstractTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -53,85 +55,40 @@ public class JslEntityDefaultValue2PsmPrimitiveAccessorTest extends AbstractTest
 
         transform();
 
-        assertBooleanDefault("Boolean", "LiteralEntity", "booleanLiteral", "true");
-        assertNumericDefault("Integer", "LiteralEntity", "intLiteral", "1");
-        assertNumericDefault("Decimal", "LiteralEntity", "decimalLiteral", "1.23");
-        assertStringDefault("String", "LiteralEntity", "escapedStringLiteral", "\"escaped\nstring\"");
-        assertStringDefault("String", "LiteralEntity", "rawStringLiteral", "\"raw\\\\nstring\"");
-        assertDateDefault("Date", "LiteralEntity", "dateLiteral", "`2020-02-18`");
-        assertTimestampDefault("Timestamp", "LiteralEntity", "timeStampLiteral", "`2020-02-18T10:11:12Z`");
-        assertTimeDefault("Time", "LiteralEntity", "timeLiteral", "`23:59:59`");
+        assertDefault("LiteralEntity", "booleanLiteral", "true", () -> assertBooleanType("Boolean"));
+        assertDefault("LiteralEntity", "intLiteral", "1", () -> assertNumericType("Integer"));
+        assertDefault("LiteralEntity", "decimalLiteral", "1.23", () -> assertNumericType("Decimal"));
+        assertDefault("LiteralEntity", "escapedStringLiteral", "\"escaped\nstring\"", () -> assertStringType("String"));
+        assertDefault("LiteralEntity", "rawStringLiteral", "\"raw\\\\nstring\"", () -> assertStringType("String"));
+        assertDefault("LiteralEntity", "dateLiteral", "`2020-02-18`", () -> assertDateType("Date"));
+        assertDefault("LiteralEntity", "timeStampLiteral", "`2020-02-18T10:11:12Z`", () -> assertTimestampType("Timestamp"));
+        assertDefault("LiteralEntity", "timeLiteral", "`23:59:59`", () -> assertTimeType("Time"));
 
-        assertBooleanDefault("Boolean", "LiteralEntityWithIdentifiers", "idBooleanLiteral", "true");
-        assertNumericDefault("Integer", "LiteralEntityWithIdentifiers", "idIntLiteral", "1");
-        assertNumericDefault("Decimal", "LiteralEntityWithIdentifiers", "idDecimalLiteral", "1.23");
-        assertStringDefault("String", "LiteralEntityWithIdentifiers", "idEscapedStringLiteral", "\"escaped\nstring\"");
-        assertStringDefault("String", "LiteralEntityWithIdentifiers", "idRawStringLiteral", "\"raw\\\\nstring\"");
-        assertDateDefault("Date", "LiteralEntityWithIdentifiers", "idDateLiteral", "`2020-02-18`");
-        assertTimestampDefault("Timestamp", "LiteralEntityWithIdentifiers", "idTimeStampLiteral", "`2020-02-18T10:11:12Z`");
-        assertTimeDefault("Time", "LiteralEntityWithIdentifiers", "idTimeLiteral", "`23:59:59`");
+        assertDefault("LiteralEntityWithIdentifiers", "idBooleanLiteral", "true", () -> assertBooleanType("Boolean"));
+        assertDefault("LiteralEntityWithIdentifiers", "idIntLiteral", "1", () -> assertNumericType("Integer"));
+        assertDefault("LiteralEntityWithIdentifiers", "idDecimalLiteral", "1.23", () -> assertNumericType("Decimal"));
+        assertDefault("LiteralEntityWithIdentifiers", "idEscapedStringLiteral", "\"escaped\nstring\"", () -> assertStringType("String"));
+        assertDefault("LiteralEntityWithIdentifiers", "idRawStringLiteral", "\"raw\\\\nstring\"", () -> assertStringType("String"));
+        assertDefault("LiteralEntityWithIdentifiers", "idDateLiteral", "`2020-02-18`", () -> assertDateType("Date"));
+        assertDefault("LiteralEntityWithIdentifiers", "idTimeStampLiteral", "`2020-02-18T10:11:12Z`", () -> assertTimestampType("Timestamp"));
+        assertDefault("LiteralEntityWithIdentifiers", "idTimeLiteral", "`23:59:59`", () -> assertTimeType("Time"));
 
-        assertNumericDefault("Decimal", "ComplexDefaultsEntity", "parenthesizedField", "((1 + 2) * 3) / 4");
-        assertStringDefault("String", "ComplexDefaultsEntity", "ternaryField", "1 < 2 ? \"yes\" : \"no\"");
-        assertBooleanDefault("Boolean", "ComplexDefaultsEntity", "unaryField", "not true");
-        assertNumericDefault("Integer", "ComplexDefaultsEntity", "binaryField", "1 + 2");
+        assertDefault("ComplexDefaultsEntity", "parenthesizedField", "((1 + 2) * 3) / 4", () -> assertNumericType("Decimal"));
+        assertDefault("ComplexDefaultsEntity", "ternaryField", "1 < 2 ? \"yes\" : \"no\"", () -> assertStringType("String"));
+        assertDefault("ComplexDefaultsEntity", "unaryField", "not true", () -> assertBooleanType("Boolean"));
+        assertDefault("ComplexDefaultsEntity", "binaryField", "1 + 2", () -> assertNumericType("Integer"));
 
         final PrimitiveAccessor withDefaultErrorField = assertUnmappedTransferObjectAttribute("ErrorWithDefaults", "withDefault").getDefaultValue();
         assertEquals(assertStringType("String"), withDefaultErrorField.getDataType());
         assertEquals("\"Hello!\"", withDefaultErrorField.getGetterExpression().getExpression());
     }
 
-    private void assertBooleanDefault(String type, String toName, String attrName, String defaultValue) {
-//        assertDataProperty("_LiteralEntity", "_booleanLiteral_default_LiteralEntity");
-//        assertEquals(assertBooleanType("Boolean"), assertDataProperty("_LiteralEntity", "_booleanLiteral_default_LiteralEntity").getDataType());
-//        final PrimitiveAccessor booleanLiteral = assertMappedTransferObjectAttribute("LiteralEntity", "booleanLiteral").getDefaultValue();
-//        assertEquals(assertBooleanType("Boolean"), booleanLiteral.getDataType());
-//        assertEquals("true", booleanLiteral.getGetterExpression().getExpression());
-
+    private void assertDefault(String toName, String attrName, String defaultValue, Supplier<? extends FlatPrimitiveType> call) {
         assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName);
-        assertEquals(assertBooleanType(type), assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName).getDataType());
+        assertEquals(call.get(), assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName).getDataType());
         final PrimitiveAccessor literal = assertMappedTransferObjectAttribute(toName, attrName).getDefaultValue();
-        assertEquals(assertBooleanType(type), literal.getDataType());
+        assertEquals(call.get(), literal.getDataType());
         assertEquals(defaultValue, literal.getGetterExpression().getExpression());
     }
 
-    private void assertNumericDefault(String type, String toName, String attrName, String defaultValue) {
-        assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName);
-        assertEquals(assertNumericType(type), assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName).getDataType());
-        final PrimitiveAccessor literal = assertMappedTransferObjectAttribute(toName, attrName).getDefaultValue();
-        assertEquals(assertNumericType(type), literal.getDataType());
-        assertEquals(defaultValue, literal.getGetterExpression().getExpression());
-    }
-
-    private void assertStringDefault(String type, String toName, String attrName, String defaultValue) {
-        assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName);
-        assertEquals(assertStringType(type), assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName).getDataType());
-        final PrimitiveAccessor literal = assertMappedTransferObjectAttribute(toName, attrName).getDefaultValue();
-        assertEquals(assertStringType(type), literal.getDataType());
-        assertEquals(defaultValue, literal.getGetterExpression().getExpression());
-    }
-
-    private void assertDateDefault(String type, String toName, String attrName, String defaultValue) {
-        assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName);
-        assertEquals(assertDateType(type), assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName).getDataType());
-        final PrimitiveAccessor literal = assertMappedTransferObjectAttribute(toName, attrName).getDefaultValue();
-        assertEquals(assertDateType(type), literal.getDataType());
-        assertEquals(defaultValue, literal.getGetterExpression().getExpression());
-    }
-
-    private void assertTimestampDefault(String type, String toName, String attrName, String defaultValue) {
-        assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName);
-        assertEquals(assertTimestampType(type), assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName).getDataType());
-        final PrimitiveAccessor literal = assertMappedTransferObjectAttribute(toName, attrName).getDefaultValue();
-        assertEquals(assertTimestampType(type), literal.getDataType());
-        assertEquals(defaultValue, literal.getGetterExpression().getExpression());
-    }
-
-    private void assertTimeDefault(String type, String toName, String attrName, String defaultValue) {
-        assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName);
-        assertEquals(assertTimeType(type), assertDataProperty("_" + toName, "_" + attrName + "_default_" + toName).getDataType());
-        final PrimitiveAccessor literal = assertMappedTransferObjectAttribute(toName, attrName).getDefaultValue();
-        assertEquals(assertTimeType(type), literal.getDataType());
-        assertEquals(defaultValue, literal.getGetterExpression().getExpression());
-    }
 }
--- judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/error/JslErrorDeclaration2PsmUnmappedTransferObjectTypeTest.java
+++ judo-tatami-jsl-jsl2psm/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/error/JslErrorDeclaration2PsmUnmappedTransferObjectTypeTest.java
@@ -2,22 +2,18 @@ package hu.blackbelt.judo.tatami.jsl.jsl2psm.error;
 
 import hu.blackbelt.epsilon.runtime.execution.api.Log;
 import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
-import hu.blackbelt.judo.meta.psm.service.TransferAttribute;
-import hu.blackbelt.judo.meta.psm.service.UnmappedTransferObjectType;
-import hu.blackbelt.judo.meta.psm.type.NumericType;
-import hu.blackbelt.judo.meta.psm.type.StringType;
+import hu.blackbelt.judo.meta.psm.type.FlatPrimitiveType;
 import hu.blackbelt.judo.tatami.jsl.jsl2psm.AbstractTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -49,35 +45,41 @@ public class JslErrorDeclaration2PsmUnmappedTransferObjectTypeTest extends Abstr
 
     @Test
     void testCreateErrorType() throws Exception {
-        testName = "TestCreateErrorType";
+        testName = "ErrorTypeCreateModel";
 
-        jslModel = parser.getModelFromStrings(
+        jslModel = parser.getModelFromFiles(
                 "ErrorTypeCreateModel",
-                List.of("model ErrorTypeCreateModel\n" +
-                        "\n" +
-                        "type numeric Integer precision 9  scale 0\n" +
-                        "type string String max-length 128\n" +
-                        "\n" +
-                        "error MyError {\n" +
-                        "\tfield required Integer code\n" +
-                        "\tfield String msg\n" +
-                        "}"
-                )
+                List.of(new File("src/test/resources/error/ErrorTestModel.jsl"))
         );
 
         transform();
 
-        assertEquals(1, getUnmappedTransferObjectTypes().size());
+        assertEquals(3, getUnmappedTransferObjectTypes().size());
 
         assertUnmappedTransferObject("MyError");
+        assertUnmappedTransferObject("OtherError");
+        assertUnmappedTransferObject("YetAnotherError");
 
-        assertUnmappedTransferObjectAttribute("MyError", "code");
+        assertUnmappedTransferObjectAttribute("OtherError", "code");
         assertTrue(assertUnmappedTransferObjectAttribute("MyError", "code").isRequired());
         assertEquals(assertNumericType("Integer"), assertUnmappedTransferObjectAttribute("MyError", "code").getDataType());
 
-        assertUnmappedTransferObjectAttribute("MyError", "msg");
-        assertFalse(assertUnmappedTransferObjectAttribute("MyError", "msg").isRequired());
-        assertEquals(assertStringType("String"), assertUnmappedTransferObjectAttribute("MyError", "msg").getDataType());
+        assertErrorData("MyError", "code", true, () -> assertNumericType("Integer"));
+        assertErrorData("MyError", "msg", false, () -> assertStringType("String"));
 
+        assertErrorData("OtherError", "code", true, () -> assertNumericType("Integer"));
+        assertErrorData("OtherError", "msg", false, () -> assertStringType("String"));
+        assertErrorData("OtherError", "other", false, () -> assertStringType("String"));
+
+        assertErrorData("YetAnotherError", "code", true, () -> assertNumericType("Integer"));
+        assertErrorData("YetAnotherError", "msg", false, () -> assertStringType("String"));
+        assertErrorData("YetAnotherError", "other", false, () -> assertStringType("String"));
+        assertErrorData("YetAnotherError", "someField", false, () -> assertStringType("String"));
+    }
+
+    private void assertErrorData(String toName, String attrName, boolean isRequired, Supplier<? extends FlatPrimitiveType> call) {
+        assertUnmappedTransferObjectAttribute(toName, attrName);
+        assertEquals(isRequired, assertUnmappedTransferObjectAttribute(toName, attrName).isRequired());
+        assertEquals(call.get(), assertUnmappedTransferObjectAttribute(toName, attrName).getDataType());
     }
 }
