4527926036cc8da27cd4903920f69d87771cbb33
gprivi <gabor.privitzky@blackbelt.hu>
2022-10-19 01:14:33 +0200

    JNG-4179 min, max, sum, avg, first, last, front, back with lambda

--- judo-tatami-jsl-jsl2psm/src/main/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/JslExpressionToJqlExpression.java
+++ judo-tatami-jsl-jsl2psm/src/main/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/JslExpressionToJqlExpression.java
@@ -620,6 +620,25 @@ public class JslExpressionToJqlExpression {
         if (it == null) {
             return null;            
         }
+        
+        String functionName = it.getFunctionDeclarationReference().getName();
+
+        if (functionName.equals("first")) {
+            return "head" + "(" + it.getLambdaArgument().getName() + " | " + getJql(it.getExpression()) + ")";
+        }
+
+        if (functionName.equals("last")) {
+            return "head" + "(" + it.getLambdaArgument().getName() + " | " + getJql(it.getExpression()) + " DESC)";
+        }
+
+        if (functionName.equals("front")) {
+            return "tail" + "(" + it.getLambdaArgument().getName() + " | " + getJql(it.getExpression()) + " DESC)";
+        }
+
+        if (functionName.equals("back")) {
+            return "tail" + "(" + it.getLambdaArgument().getName() + " | " + getJql(it.getExpression()) + ")";
+        }
+
         return it.getFunctionDeclarationReference().getName() + (it.getLambdaArgument() != null 
                 ? ("(" + it.getLambdaArgument().getName() + " | " + getJql(it.getExpression()) + ")") : "()") ;
    }
