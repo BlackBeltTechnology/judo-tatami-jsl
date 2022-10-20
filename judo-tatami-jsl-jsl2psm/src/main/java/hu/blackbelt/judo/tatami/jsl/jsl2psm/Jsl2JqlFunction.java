3a5aa52c3ac8f3fa64fa5d6383df06ef228cc1ac
bencegelei <gelei.bence@gmail.com>
2022-10-20 16:24:24 +0200

    JNG-4169 Ilike added

--- judo-tatami-jsl-jsl2psm/src/main/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/Jsl2JqlFunction.java
+++ judo-tatami-jsl-jsl2psm/src/main/java/hu/blackbelt/judo/tatami/jsl/jsl2psm/Jsl2JqlFunction.java
@@ -138,8 +138,9 @@ public class Jsl2JqlFunction {
                     .put("position", ImmutableList.of(ImmutableList.of(
                             ParameterValue.builder().name("substring").build())))
                     .put("like", ImmutableList.of(ImmutableList.of(
-                            ParameterValue.builder().name("pattern").build(),
-							ParameterValue.builder().name("exact").mandatory(false).build())))
+                            ParameterValue.builder().name("pattern").build())))
+                    .put("ilike", ImmutableList.of(ImmutableList.of(
+                            ParameterValue.builder().name("pattern").build())))
                     .put("lpad", ImmutableList.of(ImmutableList.of(
                             ParameterValue.builder().name("size").build(),
                             ParameterValue.builder().name("padstring").mandatory(false).build())))
