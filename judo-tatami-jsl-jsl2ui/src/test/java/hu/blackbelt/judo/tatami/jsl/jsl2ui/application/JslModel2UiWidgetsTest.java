f0d965954fc59d34c2216269b2c6db0fca0120d2
Norbert Csaba Herczeg <noherczeg@gmail.com>
2024-07-22 16:53:12 +0200

    JNG-5855 test groups and tabs

--- judo-tatami-jsl-jsl2ui/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2ui/application/JslModel2UiWidgetsTest.java
+++ judo-tatami-jsl-jsl2ui/src/test/java/hu/blackbelt/judo/tatami/jsl/jsl2ui/application/JslModel2UiWidgetsTest.java
@@ -1,7 +1,7 @@
 package hu.blackbelt.judo.tatami.jsl.jsl2ui.application;
 
 import hu.blackbelt.judo.meta.jsl.runtime.JslParser;
-import hu.blackbelt.judo.meta.ui.Application;
+import hu.blackbelt.judo.meta.ui.*;
 import hu.blackbelt.judo.tatami.jsl.jsl2ui.AbstractTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,7 +13,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class JslModel2UiWidgetsTest extends AbstractTest {
@@ -80,7 +80,7 @@ public class JslModel2UiWidgetsTest extends AbstractTest {
                         field String stringDerived <= u.string;
                     }
 
-                    group level3 width:6 frame:true icon:"dog" label:"Level 3" orientation:horizontal {
+                    group level22 width:6 frame:true icon:"dog" label:"Level 2 - 2" orientation:horizontal {
                         field Boolean booleanDerived <= u.boolean;
                         field Date dateDerived <= u.date;
                         field Numeric numericDerived <= u.numeric;
@@ -91,7 +91,7 @@ public class JslModel2UiWidgetsTest extends AbstractTest {
                             field Time timeDerived <= u.time;
                         }
 
-                        group tab2 label:"Tab2" icon:"numbers" h-align:center {
+                        group tab2 label:"Tab2" icon:"numbers" h-align:right {
                             field Timestamp timestampDerived <= u.timestamp;
                             field MyEnum mappedEnum <= u.`enum` bind default:MyEnum#Crazy;
                         }
@@ -111,5 +111,96 @@ public class JslModel2UiWidgetsTest extends AbstractTest {
         assertEquals(1, apps.size());
 
         Application app = apps.get(0);
+
+        List<PageContainer> pageContainers = app.getPageContainers();
+
+        assertEquals(2, pageContainers.size());
+
+        PageContainer dashboard = pageContainers.stream().filter(c -> c.getName().equals("BasicWidgetsTestModel::WidgetsActor::Dashboard")).findFirst().orElseThrow();
+        PageContainer user = pageContainers.stream().filter(c -> c.getName().equals("BasicWidgetsTestModel::WidgetsActor::user::PageContainer")).findFirst().orElseThrow();
+
+        // Dashboard
+        assertEquals(0, dashboard.getChildren().size());
+
+        // User
+
+        assertEquals(1, user.getChildren().size());
+
+        // Root Flex
+        Flex rootFlex = (Flex) user.getChildren().get(0);
+
+        assertEquals("UserView", rootFlex.getName());
+        assertNull(rootFlex.getLabel());
+        assertEquals(12, rootFlex.getCol());
+        assertEquals(Axis.VERTICAL, rootFlex.getDirection());
+        assertEquals(1, rootFlex.getChildren().size());
+
+        // level1
+
+        Flex level1 = (Flex) rootFlex.getChildren().get(0);
+
+        assertEquals("level1", level1.getName());
+        assertEquals("Yo", level1.getLabel());
+        assertEquals("text", level1.getIcon().getIconName());
+        assertEquals(12, level1.getCol());
+        assertEquals(Axis.VERTICAL, level1.getDirection());
+        assertEquals(3, level1.getChildren().size());
+
+        // level2
+
+        Flex level2 = (Flex) level1.getChildren().stream().filter(c -> c.getName().equals("level2")).findFirst().orElseThrow();
+
+        assertEquals("level2", level2.getName());
+        assertEquals("Level 2", level2.getLabel());
+        assertEquals("unicorn", level2.getIcon().getIconName());
+        assertEquals(12, level2.getCol());
+        assertEquals(Axis.VERTICAL, level2.getDirection());
+        assertNotNull(level2.getFrame());
+        assertNotNull(level2.getStretch());
+        assertEquals(0, level2.getChildren().size());
+
+        // level2 - 2
+
+        Flex level22 = (Flex) level1.getChildren().stream().filter(c -> c.getName().equals("level22")).findFirst().orElseThrow();
+
+        assertEquals("level22", level22.getName());
+        assertEquals("Level 2 - 2", level22.getLabel());
+        assertEquals("dog", level22.getIcon().getIconName());
+        assertEquals(6, level22.getCol());
+        assertEquals(Axis.HORIZONTAL, level22.getDirection());
+        assertEquals(0, level22.getChildren().size());
+
+        // tabs 0
+
+        TabController tabs0 = (TabController) level1.getChildren().stream().filter(c -> c.getName().equals("tabs0")).findFirst().orElseThrow();
+
+        assertEquals("tabs0", tabs0.getName());
+        assertEquals(6, tabs0.getCol());
+        assertEquals(TabOrientation.HORIZONTAL, tabs0.getOrientation());
+        assertEquals(2, tabs0.getTabs().size());
+
+        // tab1
+
+        Tab tab1 = tabs0.getTabs().stream().filter(t -> t.getName().equals("tab1")).findFirst().orElseThrow();
+        Flex tab1Element = (Flex) tab1.getElement();
+
+        assertEquals("tab1", tab1Element.getName());
+        assertEquals(12, tab1Element.getCol());
+        assertEquals("Tab1", tab1Element.getLabel());
+        assertEquals("numbers", tab1Element.getIcon().getIconName());
+        assertEquals(Axis.VERTICAL, tab1Element.getDirection());
+        assertEquals(CrossAxisAlignment.START, tab1Element.getCrossAxisAlignment());
+
+        // tab2
+
+        Tab tab2 = tabs0.getTabs().stream().filter(t -> t.getName().equals("tab2")).findFirst().orElseThrow();
+        Flex tab2Element = (Flex) tab2.getElement();
+
+        assertEquals("tab2", tab2Element.getName());
+        assertEquals(12, tab2Element.getCol());
+        assertEquals("Tab2", tab2Element.getLabel());
+        assertEquals("numbers", tab2Element.getIcon().getIconName());
+        assertEquals(Axis.VERTICAL, tab2Element.getDirection());
+        assertEquals(CrossAxisAlignment.END, tab2Element.getCrossAxisAlignment());
     }
 }
